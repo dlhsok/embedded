c10e8c16582931e91658225b59e3261fdbf16e19
waynenb <zhangwei@supcon.com>
2015-05-27 21:57:09 +0800

    Init files for Altera Cyclone IV Demo

---
+++ DE0_Nano_GOLDEN_TOP/sopc_add_qip_file.tcl
@@ -0,0 +1,3 @@
+project_open -current_revision "E:/zhangwei/WorkDoc/FPGA/DE0_Nano_v1.1.0_CDROM/Demonstration/DE0_Nano_GOLDEN_TOP/DE0_Nano.qpf"
+set_global_assignment -name QIP_FILE "cpu0.qip"
+project_close
